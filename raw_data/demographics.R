61530873d592b9bfa358e221f447bec02a93afae
Alex Spurrier <alex.spurrier@gmail.com>
2016-10-14 13:45:43 -0400

    update student race demographics w/ 2016 data, update documentation.

--- raw_data/demographics.R
+++ raw_data/demographics.R
@@ -25,15 +25,25 @@ library(stringr)
 
 source("raw_data/select_level.R")
 
+
+# create function to convert chars to nums, handling "," in 1000+ numbers
+char_to_num <- function(string = ""){
+  as.numeric(str_replace_all(string, ",", ""))
+}
+
+
 demographics12 <- read_excel("raw_data/data12/LEARNING_ENVIRONMENT_STUDENTS-TEACHERS.xlsx")
 demographics13 <- read_excel("raw_data/data13/LEARNING_ENVIRONMENT_STUDENTS-TEACHERS.xlsx")
 demographics14 <- read_excel("raw_data/data14/LEARNING_ENVIRONMENT_STUDENTS-TEACHERS.xlsx")
 demographics15 <- read_excel("raw_data/data15/LEARNING_ENVIRONMENT_STUDENTS-TEACHERS.xlsx")
+demographics16 <- read_excel("raw_data/data16/LEARNING_ENVIRONMENT_STUDENTS-TEACHERS.xlsx",
+                             sheet = 2)
+
 
 # clean race count data ####
 
 # filter out unneeded columns, rename columns
-race12 <- demographics12 %>%
+race12_count <- demographics12 %>%
   select(SCH_CD, DIST_NAME, SCH_NAME, SCH_YEAR,
          MEMBERSHIP_WHITE_CNT, MEMBERSHIP_BLACK_CNT, MEMBERSHIP_HISPANIC_CNT,
          MEMBERSHIP_ASIAN_CNT,  MEMBERSHIP_AIAN_CNT, MEMBERSHIP_HAWAIIAN_CNT,
@@ -44,7 +54,7 @@ race12 <- demographics12 %>%
          Asian = MEMBERSHIP_ASIAN_CNT, AIAN = MEMBERSHIP_AIAN_CNT,
          Hawaiian = MEMBERSHIP_HAWAIIAN_CNT, Other = MEMBERSHIP_OTHER_CNT)
 
-race13 <- demographics13 %>%
+race13_count <- demographics13 %>%
   select(SCH_CD, DIST_NAME, SCH_NAME, SCH_YEAR,
          ENROLLMENT_WHITE_CNT, ENROLLMENT_BLACK_CNT, ENROLLMENT_HISPANIC_CNT,
          ENROLLMENT_ASIAN_CNT, ENROLLMENT_AIAN_CNT, ENROLLMENT_HAWAIIAN_CNT,
@@ -55,7 +65,7 @@ race13 <- demographics13 %>%
          Asian = ENROLLMENT_ASIAN_CNT, AIAN = ENROLLMENT_AIAN_CNT,
          Hawaiian = ENROLLMENT_HAWAIIAN_CNT, Other = ENROLLMENT_OTHER_CNT)
 
-race14 <- demographics14 %>%
+race14_count <- demographics14 %>%
   select(SCH_CD, DIST_NAME, SCH_NAME, SCH_YEAR,
          MEMBERSHIP_WHITE_CNT, MEMBERSHIP_BLACK_CNT, MEMBERSHIP_HISPANIC_CNT,
          MEMBERSHIP_ASIAN_CNT,  MEMBERSHIP_AIAN_CNT, MEMBERSHIP_HAWAIIAN_CNT,
@@ -66,7 +76,18 @@ race14 <- demographics14 %>%
          Asian = MEMBERSHIP_ASIAN_CNT, AIAN = MEMBERSHIP_AIAN_CNT,
          Hawaiian = MEMBERSHIP_HAWAIIAN_CNT, TwoPlus = MEMBERSHIP_TWO_OR_MORE_CNT)
 
-race15 <- demographics15 %>%
+race15_count <- demographics15 %>%
+  select(SCH_CD, DIST_NAME, SCH_NAME, SCH_YEAR,
+         MEMBERSHIP_WHITE_CNT, MEMBERSHIP_BLACK_CNT, MEMBERSHIP_HISPANIC_CNT,
+         MEMBERSHIP_ASIAN_CNT,  MEMBERSHIP_AIAN_CNT, MEMBERSHIP_HAWAIIAN_CNT,
+         MEMBERSHIP_TWO_OR_MORE_CNT) %>%
+  rename(sch_id = SCH_CD, dist_name = DIST_NAME, sch_name = SCH_NAME,
+         year = SCH_YEAR,  White = MEMBERSHIP_WHITE_CNT,
+         Black = MEMBERSHIP_BLACK_CNT, Hispanic = MEMBERSHIP_HISPANIC_CNT,
+         Asian = MEMBERSHIP_ASIAN_CNT, AIAN = MEMBERSHIP_AIAN_CNT,
+         Hawaiian = MEMBERSHIP_HAWAIIAN_CNT, TwoPlus = MEMBERSHIP_TWO_OR_MORE_CNT)
+
+race16_count <- demographics16 %>%
   select(SCH_CD, DIST_NAME, SCH_NAME, SCH_YEAR,
          MEMBERSHIP_WHITE_CNT, MEMBERSHIP_BLACK_CNT, MEMBERSHIP_HISPANIC_CNT,
          MEMBERSHIP_ASIAN_CNT,  MEMBERSHIP_AIAN_CNT, MEMBERSHIP_HAWAIIAN_CNT,
@@ -78,121 +99,47 @@ race15 <- demographics15 %>%
          Hawaiian = MEMBERSHIP_HAWAIIAN_CNT, TwoPlus = MEMBERSHIP_TWO_OR_MORE_CNT)
 
 # bind race count data from all years together
-race_count <- bind_rows(race12, race13, race14, race15)
-
-# remove unneeded dataframes
-rm(race12, race13, race14, race15)
-
-# convert to long data & reformat data
-race_count %<>%
-  gather(race, count, -sch_id, -dist_name, -sch_name, -year) %>%
+# convert to long format
+# mutate columns
+race_count <- bind_rows(race12_count, race13_count, race14_count,
+                        race15_count, race16_count) %>%
+  gather(race, s_count, -sch_id, -dist_name, -sch_name, -year) %>%
   # combine TwoPlus and Other - term changed in 2014, convert to factor
-  mutate(year = factor(year, levels = c("20112012", "20122013", "20132014", "20142015"),
-                       labels = c("2011-2012", "2012-2013", "2013-2014", "2014-2015")),
-         race = factor(str_replace_all(race, "Other", "TwoPlus"),
-                      levels = c("White", "Black", "Hispanic", "TwoPlus", "Asian",
-                                  "AIAN", "Hawaiian"),
-                      labels = c("White","Black", "Hispanic", "Two or More/Other",
-                                 "Asian","American Indian/Alaska Native",
-                                 "Hawaiian/Pacific Islander")),
-         count = as.numeric(str_replace_all(count, ",", ""))) %>%
-  filter(!is.na(count))
-
-# select state data
-state_race_count <- select_state(race_count)
-
-# select district data
-dist_race_count <- select_dist(race_count)
-
-# select school data
-sch_race_count <- select_sch(race_count)
-
-# use race count data for package ####
-use_data(state_race_count, overwrite = TRUE)
-use_data(dist_race_count, overwrite = TRUE)
-use_data(sch_race_count, overwrite = TRUE)
-
-# clean race pct data ####
-
-# filter out unneeded columns, rename columns
-race12_pct <- demographics12 %>%
-  select(SCH_CD, DIST_NAME, SCH_NAME, SCH_YEAR,
-         MEMBERSHIP_WHITE_PCT, MEMBERSHIP_BLACK_PCT, MEMBERSHIP_HISPANIC_PCT,
-         MEMBERSHIP_ASIAN_PCT,  MEMBERSHIP_AIAN_PCT, MEMBERSHIP_HAWAIIAN_PCT,
-         MEMBERSHIP_OTHER_PCT) %>%
-  rename(sch_id = SCH_CD, dist_name = DIST_NAME, sch_name = SCH_NAME,
-         year = SCH_YEAR, White = MEMBERSHIP_WHITE_PCT,
-         Black = MEMBERSHIP_BLACK_PCT, Hispanic = MEMBERSHIP_HISPANIC_PCT,
-         Asian = MEMBERSHIP_ASIAN_PCT, AIAN = MEMBERSHIP_AIAN_PCT,
-         Hawaiian = MEMBERSHIP_HAWAIIAN_PCT, Other = MEMBERSHIP_OTHER_PCT)
-
-race13_pct <- demographics13 %>%
-  select(SCH_CD, DIST_NAME, SCH_NAME, SCH_YEAR,
-         ENROLLMENT_WHITE_PCT, ENROLLMENT_BLACK_PCT, ENROLLMENT_HISPANIC_PCT,
-         ENROLLMENT_ASIAN_PCT, ENROLLMENT_AIAN_PCT, ENROLLMENT_HAWAIIAN_PCT,
-         ENROLLMENT_OTHER_PCT) %>%
-  rename(sch_id = SCH_CD, dist_name = DIST_NAME, sch_name = SCH_NAME,
-         year = SCH_YEAR, White = ENROLLMENT_WHITE_PCT,
-         Black = ENROLLMENT_BLACK_PCT, Hispanic = ENROLLMENT_HISPANIC_PCT,
-         Asian = ENROLLMENT_ASIAN_PCT, AIAN = ENROLLMENT_AIAN_PCT,
-         Hawaiian = ENROLLMENT_HAWAIIAN_PCT, Other = ENROLLMENT_OTHER_PCT)
-
-race14_pct <- demographics14 %>%
-  select(SCH_CD, DIST_NAME, SCH_NAME, SCH_YEAR,
-         MEMBERSHIP_WHITE_PCT, MEMBERSHIP_BLACK_PCT, MEMBERSHIP_HISPANIC_PCT,
-         MEMBERSHIP_ASIAN_PCT,  MEMBERSHIP_AIAN_PCT, MEMBERSHIP_HAWAIIAN_PCT,
-         MEMBERSHIP_TWO_OR_MORE_PCT) %>%
-  rename(sch_id = SCH_CD, dist_name = DIST_NAME, sch_name = SCH_NAME,
-         year = SCH_YEAR,  White = MEMBERSHIP_WHITE_PCT,
-         Black = MEMBERSHIP_BLACK_PCT, Hispanic = MEMBERSHIP_HISPANIC_PCT,
-         Asian = MEMBERSHIP_ASIAN_PCT, AIAN = MEMBERSHIP_AIAN_PCT,
-         Hawaiian = MEMBERSHIP_HAWAIIAN_PCT, TwoPlus = MEMBERSHIP_TWO_OR_MORE_PCT)
-
-race15_pct <- demographics15 %>%
-  select(SCH_CD, DIST_NAME, SCH_NAME, SCH_YEAR,
-         MEMBERSHIP_WHITE_PCT, MEMBERSHIP_BLACK_PCT, MEMBERSHIP_HISPANIC_PCT,
-         MEMBERSHIP_ASIAN_PCT,  MEMBERSHIP_AIAN_PCT, MEMBERSHIP_HAWAIIAN_PCT,
-         MEMBERSHIP_TWO_OR_MORE_PCT) %>%
-  rename(sch_id = SCH_CD, dist_name = DIST_NAME, sch_name = SCH_NAME,
-         year = SCH_YEAR,  White = MEMBERSHIP_WHITE_PCT,
-         Black = MEMBERSHIP_BLACK_PCT, Hispanic = MEMBERSHIP_HISPANIC_PCT,
-         Asian = MEMBERSHIP_ASIAN_PCT, AIAN = MEMBERSHIP_AIAN_PCT,
-         Hawaiian = MEMBERSHIP_HAWAIIAN_PCT, TwoPlus = MEMBERSHIP_TWO_OR_MORE_PCT)
-
-# bind race count data from all years together
-race_pct <- bind_rows(race12_pct, race13_pct, race14_pct, race15_pct)
-
-# remove unneeded dataframes
-rm(race12_pct, race13_pct, race14_pct, race15_pct)
-
-# convert to long data & reformat data
-race_pct %<>%
-  gather(race, pct, -sch_id, -dist_name, -sch_name, -year) %>%
-  # combine TwoPlus and Other - term changed in 2014, convert to factor
-  mutate(year = factor(year, levels = c("20112012", "20122013", "20132014", "20142015"),
-                       labels = c("2011-2012", "2012-2013", "2013-2014", "2014-2015")),
+  mutate(s_count = char_to_num(s_count),
+         year = factor(year, levels = c("20112012", "20122013", "20132014",
+                                        "20142015", "20152016"),
+                       labels = c("2011-2012", "2012-2013", "2013-2014",
+                                  "2014-2015", "2015-2016")),
          race = factor(str_replace_all(race, "Other", "TwoPlus"),
                        levels = c("White", "Black", "Hispanic", "TwoPlus", "Asian",
                                   "AIAN", "Hawaiian"),
                        labels = c("White","Black", "Hispanic", "Two or More/Other",
                                   "Asian","American Indian/Alaska Native",
-                                  "Hawaiian/Pacific Islander")),
-         pct = as.numeric(str_replace_all(pct, "%", "")) / 100) %>%
-  filter(!is.na(pct))
+                                  "Hawaiian/Pacific Islander"))) %>%
+  filter(!is.na(s_count)) %>%
+  group_by(sch_id, dist_name, sch_name, year) %>%
+  mutate(calc_pct = s_count/sum(s_count)) %>%
+  ungroup()
+
+# remove unneeded dataframes
+rm(race12_count, race13_count, race14_count,
+   race15_count, race16_count)
+
+# select state, dist, and sch level race data ####
 
 # select state data
-state_race_pct <- select_state(race_pct)
+race_state <- select_state(race_count)
 
 # select district data
-dist_race_pct <- select_dist(race_pct)
+race_dist <- select_dist(race_count)
 
 # select school data
-sch_race_pct <- select_sch(race_pct)
+race_sch <- select_sch(race_count)
 
-# use race pct data for package ####
-use_data(state_race_pct, overwrite = TRUE)
-use_data(dist_race_pct, overwrite = TRUE)
-use_data(sch_race_pct, overwrite = TRUE)
+# use race data for package ####
+use_data(race_state, overwrite = TRUE)
+use_data(race_dist, overwrite = TRUE)
+use_data(race_sch, overwrite = TRUE)
 
 # clean teacher data ####
 
