3531f4a891955f61b0a50e80bc435c7cc5a4d352
Alex Spurrier <alex.spurrier@gmail.com>
2016-10-19 15:34:32 -0400

    add ccr data; update documentation

---
+++ R/ccr.R
@@ -0,0 +1,54 @@
+#' State College/Career Readiness Data for Kentucky Public Schools, 2012-2016
+#'
+#' A dataset with college/career ready data
+#'
+#' @format A dataframe with 85 rows and 6 variables:
+#' \describe{
+#'   \item{sch_id}{ID number to identify schools & districts}
+#'   \item{dist_name}{District name - in this case, it is always "State Total"}
+#'   \item{year}{School year}
+#'   \item{student_group}{Student subgroup}
+#'   \item{ccr_pct}{Percent of students college and/or career ready}
+#'   \item{college_ready_pct}{Percent of students college ready}
+#' }
+#'
+#' @source \url{https://applications.education.ky.gov/src/DataSets.aspx}
+
+"ccr_state"
+
+#' District College/Career Readiness Data for Kentucky Public Schools, 2012-2016
+#'
+#' A dataset with college/career ready data
+#'
+#' @format A dataframe with 14773 rows and 6 variables:
+#' \describe{
+#'   \item{sch_id}{ID number to identify schools & districts}
+#'   \item{dist_name}{District name}
+#'   \item{year}{School year}
+#'   \item{student_group}{Student subgroup}
+#'   \item{ccr_pct}{Percent of students college and/or career ready}
+#'   \item{college_ready_pct}{Percent of students college ready}
+#' }
+#'
+#' @source \url{https://applications.education.ky.gov/src/DataSets.aspx}
+
+"ccr_dist"
+
+#' School College/Career Readiness Data for Kentucky Public Schools, 2012-2016
+#'
+#' A dataset with college/career ready data
+#'
+#' @format A dataframe with 19448 rows and 7 variables:
+#' \describe{
+#'   \item{sch_id}{ID number to identify schools & districts}
+#'   \item{dist_name}{District name}
+#'   \item{sch_name}{School name}
+#'   \item{year}{School year}
+#'   \item{student_group}{Student subgroup}
+#'   \item{ccr_pct}{Percent of students college and/or career ready}
+#'   \item{college_ready_pct}{Percent of students college ready}
+#' }
+#'
+#' @source \url{https://applications.education.ky.gov/src/DataSets.aspx}
+
+"ccr_sch"
